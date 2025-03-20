fad77da25b40edc08a853e655f0875b7655bc2f5
kseniia-shlenskaia <kseniia.shlenskaia@gmail.com>
2025-03-20 19:19:56 +0200

    fix

--- moduuli-2/2_tehtava.py
+++ moduuli-2/2_tehtava.py
@@ -1,8 +1,5 @@
-# import math
-#
-# circleRadius = float(input('Anna ympyrän säde: '))
-# circleArea = math.pi * (circleRadius**2)
-# print(f'Ympyrän pinta-ala on: {circleArea:.2f}')
+import math
 
-names = ["Viivi", "Ahmed", "Pekka", "Olga", "Mary"]
-print(names[:2])
+circleRadius = float(input('Anna ympyrän säde: '))
+circleArea = math.pi * (circleRadius**2)
+print(f'Ympyrän pinta-ala on: {circleArea:.2f}')
